29276b8f75fbc3208fbf22ac52851e5dfe1404e1
nikita_panch <pnchnk.nikita@gmail.com>
2022-08-09 16:05:02 +0300

    remove pc

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,36 @@ $('#owl-team').owlCarousel({
         }
     }
 });
+
+$('#owl-feedback').owlCarousel({
+    loop:true,
+    margin:25,
+    nav:false,
+    responsive:{
+        0:{
+            items: 1
+        },
+        460:{
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+});
+
+const $owlNext = document.querySelector('.owl-next');
+const $owlPrev = document.querySelector('.owl-prev');
+
+//  $('.owl-next').click(function() {
+//         owl.trigger('next.owl.carousel');
+//     })
+//     // Go to the previous item
+//     $('.owl-prev').click(function() {
+//         // With optional speed parameter
+//         // Parameters has to be in square bracket '[]'
+//         owl.trigger('prev.owl.carousel', [300]);
+//     })
+
+   
+    
